382cb18d87e96e252b9d240532ed7e0b991f6f09
yangming <472102644@qq.com>
2020-09-27 16:28:16 +0800

    init

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,21 @@
-// const { join } = require('path')
-module.exports = ({ file: { dirname, basename } }) => {
-  const designWidth = 375
-  return {
-    plugins: [
-      require('autoprefixer'),
-      require('postcss-px-to-viewport')({
-        unitToConvert: 'px',
-        viewportWidth: designWidth,
-        unitPrecision: 3,
-        viewportUnit: 'vw',
-        fontViewportUnit: 'vw',
-        minPixelValue: 1
-        // mediaQuery: true,
-        // include: /src\/views\/map\//,
-      })
-    ]
-  }
-}
+/**
+ * 默认不开启单位转换，如有需要，自行打开注释
+ */
+// module.exports = ({ file: { dirname, basename } }) => {
+//   const designWidth = 375
+//   return {
+//     plugins: [
+//       require('autoprefixer'),
+//       require('postcss-px-to-viewport')({
+//         unitToConvert: 'px',
+//         viewportWidth: designWidth,
+//         unitPrecision: 3,
+//         viewportUnit: 'vw',
+//         fontViewportUnit: 'vw',
+//         minPixelValue: 1
+//         // mediaQuery: true,
+//         // include: /src\/views\/map\//,
+//       })
+//     ]
+//   }
+// }
